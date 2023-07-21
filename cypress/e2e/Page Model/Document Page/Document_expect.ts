bfe1b530291d673e3c66da84bf53b3df129c649e
elzeslymai-1 <champ_psu@outlook.co.th>
2023-07-21 11:39:56 +0700

    Draft Test Script For Document Page
    ++ Change Document_Model to extends Assertion
    ++ Add Expect Result File

---
+++ cypress/e2e/Page Model/Document Page/Document_expect.ts
@@ -0,0 +1,28 @@
+export class Document_Expect{
+    // ======================================
+    //             EXPECT RESULT
+    // ======================================
+    expect_document_topic_txt = 'Documents'
+    expect_document_topic_content_txt = 'Company Documents Are Important To Be Produced As References And Confirmations In Carrying Out Activities Both Inside And Outside The Organization.'
+
+    //  DOCUMENT & DOWNLOAD
+    expect_document_download_txt = 'Document Download'
+
+    expect_present_txt = 'PRESENTATION AND FACT SHEETS SLIDES'
+    expect_present_content_txt = 'A leading intergrated blockchain-base platform and solution provider in Asia.100 M THB Fully Paid Registered Capital'
+    expect_present_download_pdf_btn_name = 'Download PDF'
+    expect_present_download_pdf_attr_target = '_blank'
+    expect_present_download_pdf_url = 'https://finweb-strapi-image-storage.s3.ap-southeast-1.amazonaws.com/finstable_presentation_bded26a9ad.pdf'
+
+    expect_company_txt = 'COMPANY'
+    expect_company_content_txt = 'Company certificate'
+    expect_company_download_pdf_btn_name = 'Download PDF'
+    expect_company_download_pdf_attr_target = '_blank'
+    expect_company_download_pdf_url = 'https://finweb-strapi-image-storage.s3.ap-southeast-1.amazonaws.com/company_certificate_799971d75b.pdf'
+
+    expect_privacy_txt = 'PRIVACY'
+    expect_privacy_content_txt = 'Finstable Company Limited (the “Company”) regonizes the importance of protectingyour personal information. (As defined below)'
+    expect_privacy_download_pdf_btn_name = 'Download PDF'
+    expect_privacy_download_pdf_attr_target = '_blank'
+    expect_privacy_download_pdf_url = 'https://finweb-strapi-image-storage.s3.ap-southeast-1.amazonaws.com/privacy_policy_eng_052ae229fe.pdf'
+}
--- cypress/e2e/Page Model/Document Page/Document_model.ts
+++ cypress/e2e/Page Model/Document Page/Document_model.ts
@@ -4,6 +4,6 @@ import { Assertion } from "../../Main Component/Assert";
 const action = new Actions()
 const assert = new Assertion()
 
-export class Document_Model{
-    
+export class Document_Model extends Assertion {
+
 }
